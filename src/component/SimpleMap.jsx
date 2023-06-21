a4cf65e90aef4af2198a9043143cbbd211f46767
ellielsewhere <114022661+ellielsewhere@users.noreply.github.com>
2023-06-20 20:03:28 -0500

    css

--- src/component/SimpleMap.jsx
+++ src/component/SimpleMap.jsx
@@ -119,7 +119,7 @@ export const SimpleMap = ({ zipCode, setPlace, place }) => {
 
       </div> */}
 
-      <div className="map-container" style={{ height: "68vh", width: "100%" }}>
+      <div className="map-container" style={{ height: "73vh", width: "100%" }}>
         <GoogleMapReact
           bootstrapURLKeys={{ key: "REMOVED" }}
           center={city.center}
